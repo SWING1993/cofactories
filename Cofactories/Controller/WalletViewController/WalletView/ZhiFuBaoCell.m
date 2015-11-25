6cae0721f0730f917f8da742c56d4e7885f359bf
赵广印 <zhaoguangyin@zhaoguangyindeiMac.local>
2015-11-25 09:35:00 +0800

    钱包

---
+++ Cofactories/Controller/WalletViewController/WalletView/ZhiFuBaoCell.m
@@ -0,0 +1,23 @@
+//
+//  ZhiFuBaoCell.m
+//  Cofactories
+//
+//  Created by 赵广印 on 15/11/24.
+//  Copyright © 2015年 宋国华. All rights reserved.
+//
+
+#import "ZhiFuBaoCell.h"
+
+@implementation ZhiFuBaoCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
