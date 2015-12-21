a2a6e9f70bd691d8d3b8eb4e4bb489a51dae8b68
赵广印 <zhaoguangyin@zhaoguangyindeiMac.local>
2015-12-21 14:41:20 +0800

    活动

--- Cofactories/Controller/MeViewController/HistoryOrder/MeHistoryOrderDetail_VC.m
+++ Cofactories/Controller/MeViewController/HistoryOrder/MeHistoryOrderDetail_VC.m
@@ -31,39 +31,39 @@ static NSString *thirdCellIdentifier = @"thirdCell";
     [self.tableView registerClass:[HistoryOrderDetailCell class] forCellReuseIdentifier:orderDetailCellIdentifier];
     [self.tableView registerClass:[HistoryOrderAddressCell class] forCellReuseIdentifier:addressCellIdentifier];
     [self.tableView registerClass:[HistoryOrderThirdCell class] forCellReuseIdentifier:thirdCellIdentifier];
-    if ([self.orderModel.payType isEqualToString:@"待付款"] && self.isBuy == YES) {
-        [self creatTableViewFooterView];
-    }
+//    if ([self.orderModel.payType isEqualToString:@"待付款"] && self.isBuy == YES) {
+//        [self creatTableViewFooterView];
+//    }
 }
 
-- (void)creatTableViewFooterView {
-    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenW, 100)];
-    lastButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    lastButton.frame = CGRectMake(20, 30, kScreenW - 40, 38);
-    [lastButton setTitle:@"去付款" forState:UIControlStateNormal];
-    lastButton.titleLabel.font = [UIFont systemFontOfSize:15.5];
-    lastButton.layer.cornerRadius = 4;
-    lastButton.clipsToBounds = YES;
-    lastButton.backgroundColor = [UIColor colorWithRed:30.0f/255.0f green:171.0f/255.0f blue:235.0f/255.0f alpha:1.0f];
-    [lastButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [lastButton addTarget:self action:@selector(actionOfPay:) forControlEvents:UIControlEventTouchUpInside];
-    [footerView addSubview:lastButton];
-    DLog(@"^^^^^^^^^%@", self.orderModel.orderNumber);
-    self.tableView.tableFooterView = footerView;
-}
-- (void)actionOfPay:(UIButton *)button {
-    DLog(@"去付款");
-    NSString *tradeNO = self.orderModel.orderNumber;
-    NSString *descriptionStr = self.orderModel.descriptions;
-    NSString *subject = [NSString stringWithFormat:@"%@ %@ X%@", self.orderModel.name, self.orderModel.category, self.orderModel.amount];
-    NSString *amountStr = self.orderModel.totalPrice;
-    if (tradeNO && descriptionStr && subject) {
-        [AlipayRequestConfig alipayWithPartner:kPartnerID seller:kSellerAccount tradeNO:tradeNO productName:subject productDescription:descriptionStr amount:amountStr notifyURL:kNotifyURL itBPay:@"30m"];
-    } else {
-        kTipAlert(@"生成订单信息失败");
-    }
-
-}
+//- (void)creatTableViewFooterView {
+//    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenW, 100)];
+//    lastButton = [UIButton buttonWithType:UIButtonTypeCustom];
+//    lastButton.frame = CGRectMake(20, 30, kScreenW - 40, 38);
+//    [lastButton setTitle:@"去付款" forState:UIControlStateNormal];
+//    lastButton.titleLabel.font = [UIFont systemFontOfSize:15.5];
+//    lastButton.layer.cornerRadius = 4;
+//    lastButton.clipsToBounds = YES;
+//    lastButton.backgroundColor = [UIColor colorWithRed:30.0f/255.0f green:171.0f/255.0f blue:235.0f/255.0f alpha:1.0f];
+//    [lastButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+//    [lastButton addTarget:self action:@selector(actionOfPay:) forControlEvents:UIControlEventTouchUpInside];
+//    [footerView addSubview:lastButton];
+//    DLog(@"^^^^^^^^^%@", self.orderModel.orderNumber);
+//    self.tableView.tableFooterView = footerView;
+//}
+//- (void)actionOfPay:(UIButton *)button {
+//    DLog(@"去付款");
+//    NSString *tradeNO = self.orderModel.orderNumber;
+//    NSString *descriptionStr = self.orderModel.descriptions;
+//    NSString *subject = [NSString stringWithFormat:@"%@ %@ X%@", self.orderModel.name, self.orderModel.category, self.orderModel.amount];
+//    NSString *amountStr = self.orderModel.totalPrice;
+//    if (tradeNO && descriptionStr && subject) {
+//        [AlipayRequestConfig alipayWithPartner:kPartnerID seller:kSellerAccount tradeNO:tradeNO productName:subject productDescription:descriptionStr amount:amountStr notifyURL:kNotifyURL itBPay:@"30m"];
+//    } else {
+//        kTipAlert(@"生成订单信息失败");
+//    }
+//
+//}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
