20983947f26f48f3c1ea2b2480125ccdc0bb1a94
赵广印 <zhaoguangyin@zhaoguangyindeiMac.local>
2015-12-22 16:01:43 +0800

    修改字体

--- Cofactories/Controller/HomeViewController/HomeDesignMarketController/ZGYDesignMarkrtController.m
+++ Cofactories/Controller/HomeViewController/HomeDesignMarketController/ZGYDesignMarkrtController.m
@@ -30,9 +30,9 @@
     self.navigationItem.title = @"设计市场";
     
     self.view.backgroundColor = [UIColor colorWithRed:251.0f/255.0f green:252.0f/255.0f blue:253.0f/255.0f alpha:1.0f];
-    NSArray *photoArray = @[@"Market-版型购买", @"Market-流行资讯", @"Market-设计者汇总"];
-    NSArray *titleArray = @[@"版型购买", @"流行资讯", @"设计者汇总"];
-    NSArray *detailTitleArray = @[@"最全版型聚集地", @"最全流行资讯聚集地", @"最全潮流设计师聚集地"];
+    NSArray *photoArray = @[@"Market-流行资讯", @"Market-设计者汇总", @"Market-版型购买"];
+    NSArray *titleArray = @[@"时尚资讯", @"设计师集中营", @"版型聚集地"];
+    NSArray *detailTitleArray = @[@"最全流行资讯聚集地", @"最全潮流设计师聚集地", @"最全版型聚集地"];
     ZGYSupplyMarketView *designView = [[ZGYSupplyMarketView alloc] initWithFrame:CGRectMake(0, 64, kScreenW, 370) photoArray:photoArray titleArray:titleArray detailTitleArray:detailTitleArray];
     designView.delegate = self;
     [self.view addSubview:designView];
@@ -46,19 +46,20 @@
 - (void)supplyMarketView:(ZGYSupplyMarketView *)supplyMarketView supplyMarketButtonTag:(NSInteger)supplyMarketButtonTag {
     switch (supplyMarketButtonTag) {
         case 0:{
-            DesignShop_VC *designShopVC = [[DesignShop_VC alloc] initWithSubrole:@"设计者" andSelecteDataDictionary:[Tools goodsSelectDataDictionaryWithIndex:4]];
-            [self.navigationController pushViewController:designShopVC animated:YES];
+            PopularMessageController *popularVC = [[PopularMessageController alloc] init];
+            [self.navigationController pushViewController:popularVC animated:YES];
 
         }
             break;
         case 1:{
-            PopularMessageController *popularVC = [[PopularMessageController alloc] init];
-            [self.navigationController pushViewController:popularVC animated:YES];
+            AllDesignController *allDesignVC = [[AllDesignController alloc] initWithSelecteDataDictionary:[Tools returenSelectDataDictionaryWithIndex:1]];
+            [self.navigationController pushViewController:allDesignVC animated:YES];
+            
         }
             break;
         case 2:{
-            AllDesignController *allDesignVC = [[AllDesignController alloc] initWithSelecteDataDictionary:[Tools returenSelectDataDictionaryWithIndex:1]];
-            [self.navigationController pushViewController:allDesignVC animated:YES];
+            DesignShop_VC *designShopVC = [[DesignShop_VC alloc] initWithSubrole:@"设计者" andSelecteDataDictionary:[Tools goodsSelectDataDictionaryWithIndex:4]];
+            [self.navigationController pushViewController:designShopVC animated:YES];
         }
             break;
         default:
